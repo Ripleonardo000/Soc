d5649f885b84f7db1090729584ce3e9831b71e35
Jordy Aguilar <jeay022@gmail.com>
2024-12-03 18:47:06 -0500

    Agregue clase que ajusta formato de fecha para su correcta representacion

---
+++ Soc/FechaHoraConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace soc
+{
+    public class FechaHoraConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string fechaIso)
+            {
+                if (DateTime.TryParse(fechaIso, null, DateTimeStyles.RoundtripKind, out DateTime fechaFormateada))
+                {
+                    return fechaFormateada.ToString("dd MMM yyyy, HH:mm", CultureInfo.InvariantCulture);
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
